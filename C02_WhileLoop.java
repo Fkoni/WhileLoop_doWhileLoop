8a2f88f86a5422c64bcdebf576cb470a9377d2fe
Fkoni <93380100+Fkoni@users.noreply.github.com>
2021-12-24 18:24:56 +0300

    Add files via upload

---
+++ C02_WhileLoop.java
@@ -0,0 +1,35 @@
+package day19_WhileLoop_doWhileLoop;
+
+import java.util.Scanner;
+
+public class C02_WhileLoop {
+
+	public static void main(String[] args) {
+		// Kullanicidan bir sayi alin 
+        // ve bu sayinin rakamlari toplamini yazdirin
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Lutfen rakamlar toplamını bulmak icin pozitif bir tam sayi giriniz : ");
+		int sayi = scan.nextInt();
+        
+		int rakamlarToplami=0;
+		int rakam=0;
+		
+		
+		while (sayi>0) {
+			rakam=sayi%10;
+			rakamlarToplami+=rakam;
+			sayi/=10;
+		}
+		System.out.println("Girdiginiz sayinin rakamlar toplami  : " +rakamlarToplami);
+		
+		scan.close();
+		
+		
+		
+		
+		
+		
+	}
+
+}
